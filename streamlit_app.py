e8c9f977e10181ac47da1757e61abfe7fd1bda68
Dr. Ernesto Lee <socrates73@gmail.com>
2025-03-01 11:45:12 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ from io import BytesIO
 import base64
 
 st.title("OpenCV Deep Learning based Face Detection")
-uploaded_file = st.file_uploader("Choose a file", type =['jpg','jpeg','png'])
+uploaded_file = st.file_uploader("Choose a file", type=['jpg', 'jpeg', 'png'])
 
 # Function for Detecting face and annotating with rectangles
 def detectFaceOpenCVDnn(net, frame, conf_threshold=0.5):
@@ -16,7 +16,7 @@ def detectFaceOpenCVDnn(net, frame, conf_threshold=0.5):
     frameHeight = frameOpencvDnn.shape[0]
     frameWidth = frameOpencvDnn.shape[1]
 
-    # Create a blob from the image and apply some preprocesing
+    # Create a blob from the image and apply some preprocessing
     blob = cv2.dnn.blobFromImage(
         frameOpencvDnn, 1.0, (300, 300), [104, 117, 123], False, False,
     )
@@ -46,7 +46,7 @@ def detectFaceOpenCVDnn(net, frame, conf_threshold=0.5):
 
 
 # Function to load the DNN model
-@st.cache(allow_output_mutation=True)
+@st.cache_resource
 def load_model():
     modelFile = "res10_300x300_ssd_iter_140000_fp16.caffemodel"
     configFile = "deploy.prototxt"
@@ -55,18 +55,18 @@ def load_model():
 
 
 # Function to generate a download link for output file
-def get_image_download_link(img,filename,text):
+def get_image_download_link(img, filename, text):
     buffered = BytesIO()
     img.save(buffered, format="JPEG")
     img_str = base64.b64encode(buffered.getvalue()).decode()
-    href =  f'<a href="data:file/txt;base64,{img_str}" download="{filename}">{text}</a>'
+    href = f'<a href="data:file/txt;base64,{img_str}" download="{filename}">{text}</a>'
     return href
 
 
 net = load_model()
 
 if uploaded_file is not None:
-    # Read the file and convert it to opencv Image
+    # Read the file and convert it to OpenCV Image
     raw_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
     opencv_image = cv2.imdecode(raw_bytes, cv2.IMREAD_COLOR)
 
@@ -77,16 +77,16 @@ if uploaded_file is not None:
     placeholders[0].text("Input Image")
 
     # Create a Slider and Get the threshold from the slider
-    conf_threshold = st.slider("SET Confidence Threshold", min_value = 0.0, max_value = 1.0, step = .01, value=0.5)
+    conf_threshold = st.slider("SET Confidence Threshold", min_value=0.0, max_value=1.0, step=0.01, value=0.5)
 
     # Call the function to get detected faces
-    out_image,_ = detectFaceOpenCVDnn(net, opencv_image, conf_threshold=conf_threshold)
+    out_image, _ = detectFaceOpenCVDnn(net, opencv_image, conf_threshold=conf_threshold)
 
     # Display Detected Faces
     placeholders[1].image(out_image, channels='BGR')
     placeholders[1].text("Output Image")
 
-    # Convert opencv image to PIL
-    out_image = Image.fromarray(out_image[:,:,::-1])
+    # Convert OpenCV image to PIL
+    out_image = Image.fromarray(out_image[:, :, ::-1])
     # Create a link for downloading the output file
     st.markdown(get_image_download_link(out_image, "face_output.jpg", 'Download Output Image'), unsafe_allow_html=True)
